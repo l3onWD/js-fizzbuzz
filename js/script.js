8a586fef819267dee7c795502b73d5bc1b326556
l3on <l3onwebdev@gmail.com>
2023-05-30 19:42:10 +0200

    moved parameters to init section

--- js/script.js
+++ js/script.js
@@ -13,47 +13,47 @@ console.log('----------- INIT -----------');
 
 
 // *** DOM ELEMENTS ***//
-const deckElem = document.getElementById('deck');
-
-
-// *** CONTROLS BAR ELEMENTS ***//
+// Controls Bar
 const controlsBarElem = document.getElementById('controls-bar');
 const fizzDividerElem = document.getElementById('fizz-divider');
 const buzzDividerElem =  document.getElementById('buzz-divider');
 const maxNumberElem =  document.getElementById('max-number');
 const generateElem =  document.getElementById('controls-generate');
 const resetElem =  document.getElementById('controls-reset');
+// Deck Container
+const deckElem = document.getElementById('deck');
 
 // ! Log Elements
 console.log('');
-console.log("# Controls Bar Elements:");
-console.log("Container: " + controlsBarElem);
-console.log("Fizz Divider: " + fizzDividerElem);
-console.log("Buzz Divider: " + buzzDividerElem);
-console.log("Max Number: " + maxNumberElem);
-console.log("Generate Button: " + generateElem);
-console.log("Reset Button: " + resetElem);
+console.log('# DOM Elements:');
+console.log('Container: ' + controlsBarElem);
+console.log('Fizz Divider: ' + fizzDividerElem);
+console.log('Buzz Divider: ' + buzzDividerElem);
+console.log('Max Number: ' + maxNumberElem);
+console.log('Generate Button: ' + generateElem);
+console.log('Reset Button: ' + resetElem);
+console.log('Deck Container: ' + deckElem);
+
+
+// *** GET DEFAULT PARAMETERS ***//
+// Dividers
+let fizzDividerValue = parseInt(fizzDividerElem.value);
+let buzzDividerValue = parseInt(buzzDividerElem.value);
+// max Number
+let maxNumberValue = parseInt(maxNumberElem.value);
+
+// ! Log Parameters
+console.log('');
+console.log('# Parameters:');
+console.log('Fizz Divider: ' + fizzDividerValue);
+console.log('Buzz Divider: ' + buzzDividerValue);
+console.log('Max Numbers: ' + maxNumberValue);
 
 
 /* -----------------------------------------
 * LOGIC
 -------------------------------------------*/
 console.log('----------- LOGIC -----------');
-
-
-// *** GET USER INPUTS ***//
-// Dividers
-const fizzDividerValue = parseInt(fizzDividerElem.value);
-const buzzDividerValue = parseInt(buzzDividerElem.value);
-// max Number
-const maxNumberValue = parseInt(maxNumberElem.value);
-
-// ! Log User Inputs
-console.log('');
-console.log("# User Inputs:");
-console.log("Fizz Divider: " + fizzDividerValue);
-console.log("Buzz Divider: " + buzzDividerValue);
-console.log("Max Numbers: " + maxNumberValue);
 
 
 // ! Log List
